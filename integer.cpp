831525c129892ec14693fcc2c44d902f68c9e8b7
tkt-2605 <126350243+tkt-2605@users.noreply.github.com>
2023-02-25 23:35:55 +0530

    Add files via upload

---
+++ integer.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a,b;
+cout<<"enter the 1st and second integer respectively";
+cin >> a >> b;
+if(a>b)
+  cout<<"integer" <<a<< "is greated then int "<<b<<endl;
+else
+  cout<<"integer " <<b<< " is greated then int "<<a<<endl;
+
+return 0;
+}
